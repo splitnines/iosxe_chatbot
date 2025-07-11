971b68c396e840df47bc0854490e10120e5c8115
splitnines <splitnines@gmail.com>
2025-07-10 20:18:22 -0700

    Refactor: added additional model to the menu.

--- lib/menu.py
+++ lib/menu.py
@@ -9,19 +9,20 @@ def menu():
         | | |_| |___) |_____/  \| |___  | |___| | | | (_| | |_| |_) | (_) | |_
        |___\___/|____/     /_/\_\_____|  \____|_| |_|\__,_|\__|_.__/ \___/ \__|
 
-        Operator Command Menu:
+         Operator Command Menu:
 
-        /c  - run a command directly on the device
-        /m  - print this menu
-        /n  - start a new context window
-        /p  - print the developer prompt
-        /q  - quit the program1
-        /r  - reload the developer prompt
-        /s  - Select model
-               1: o4-mini        2: gpt-4o
-               3: gpt-4.1-mini   4: gpt-4.1
+            /c  - run a command directly on the device
+            /m  - print this menu
+            /n  - start a new context window
+            /p  - print the developer prompt
+            /q  - quit the program1
+            /r  - reload the developer prompt
+            /s  - Select model
+                   1: o4-mini        2: gpt-4o
+                   3: gpt-4o-mini    4: gpt-4.1-mini
+                   5: gpt-4.1
 
-        To interact with the LMM just type you query.
+         To interact with the LMM just type you query.
 
         """
 
